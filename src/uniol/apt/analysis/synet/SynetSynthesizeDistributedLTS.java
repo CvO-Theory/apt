6461e6e41b0721768a6083c28be16a0b1e22c92c
Soeren Dierkes <soeren.dierkes@informatik.uni-oldenburg.de>
2013-04-03 16:34:08 +0200

    delete files created for synet, add error message if places are not separated

--- src/uniol/apt/analysis/synet/SynetSynthesizeDistributedLTS.java
+++ src/uniol/apt/analysis/synet/SynetSynthesizeDistributedLTS.java
@@ -1,3 +1,4 @@
+
 /*-
  * APT - Analysis of Petri Nets and labeled Transition systems
  * Copyright (C) 2012-2013  Members of the project group APT
@@ -45,112 +46,157 @@ import uniol.apt.module.exception.SynetNotFoundException;
  */
 public class SynetSynthesizeDistributedLTS {
 
-	private TransitionSystem ts_;
-	private PetriNet pn_;
-	private String errorMsg_;
-	private boolean location_;
+    private TransitionSystem ts_;
+    private PetriNet pn_;
+    private String errorMsg_;
+    private String separationErrorMsg_;
+    private boolean location_;
 
-	public SynetSynthesizeDistributedLTS(TransitionSystem ts) {
-		ts_ = ts;
-		location_ = false;
-	}
+    public SynetSynthesizeDistributedLTS(TransitionSystem ts) {
+        ts_ = ts;
+        location_ = false;
+    }
 
-	/**
-	 * Check if the given labeled transition system is synthesizable by Synet.
-	 *
-	 * @return <true> if synthesizable.
-	 * @throws SynetNotFoundException
-	 * @throws IOException
-	 * @throws FormatException
-	 */
-	public boolean check() throws SynetNotFoundException, IOException, FormatException {
-		SynetRenderer synetRen = new SynetRenderer();
-		String ltsSynetFormat = synetRen.render(ts_);
+    /**
+     * Check if the given labeled transition system is synthesizable by Synet.
+     *
+     * @return <true> if synthesizable.
+     * @throws SynetNotFoundException
+     * @throws IOException
+     * @throws FormatException
+     */
+    public boolean check() throws SynetNotFoundException, IOException, FormatException {
+        SynetRenderer synetRen = new SynetRenderer();
+        String ltsSynetFormat = synetRen.render(ts_);
 
-		File tmpAutFile = File.createTempFile("synetAut", ".aut");
-		BufferedWriter bw = new BufferedWriter(new FileWriter(tmpAutFile));
-		bw.write(ltsSynetFormat);
-		bw.close();
+        File tmpAutFile = File.createTempFile("synetAut", ".aut");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(tmpAutFile));
+        bw.write(ltsSynetFormat);
+        bw.close();
 
-		File tmpDisFile = File.createTempFile("synetDis", ".dis");
-		BufferedWriter bw2 = new BufferedWriter(new FileWriter(tmpDisFile));
-		bw2.write(getDisString());
-		bw2.close();
+        File tmpDisFile = File.createTempFile("synetDis", ".dis");
+        BufferedWriter bw2 = new BufferedWriter(new FileWriter(tmpDisFile));
+        bw2.write(getDisString());
+        bw2.close();
 
-		File tmpSaveFile = File.createTempFile("synetNet", ".net");
+        File tmpSaveFile = File.createTempFile("synetNet", ".net");
 
-		Process p;// -r uses a new algorithm -o creates an output file -d is the
-					// option for distributed nets with locations
-		if (location_) {
-			try {
-				p = new ProcessBuilder("synet", "-r", "-o", tmpSaveFile.getAbsolutePath(), "-d",
-						tmpDisFile.getAbsolutePath(), tmpAutFile.getAbsolutePath()).start();
-			} catch (Exception e) {
-				throw new SynetNotFoundException();
-			}
-		} else {
-			try {
-				p = new ProcessBuilder("synet", "-r", "-o", tmpSaveFile.getAbsolutePath(), tmpAutFile.getAbsolutePath()).start();
-			} catch (Exception e) {
-				throw new SynetNotFoundException();
-			}
-		}
+        Process p;// -r uses a new algorithm -o creates an output file -d is the
+                    // option for distributed nets with locations
+        if (location_) {
+            try {
+                p = new ProcessBuilder("synet", "-r", "-o", tmpSaveFile.getAbsolutePath(), "-d",
+                        tmpDisFile.getAbsolutePath(), tmpAutFile.getAbsolutePath()).start();
+            } catch (Exception e) {
+                deleteCreatedFiles(tmpAutFile, tmpDisFile, tmpSaveFile);
+                throw new SynetNotFoundException();
+            }
+        } else {
+            try {
+                p = new ProcessBuilder("synet", "-r", "-o", tmpSaveFile.getAbsolutePath(), tmpAutFile.getAbsolutePath())
+                        .start();
+            } catch (Exception e) {
+                deleteCreatedFiles(tmpAutFile, tmpDisFile, tmpSaveFile);
+                throw new SynetNotFoundException();
+            }
+        }
 
-		BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+        BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+       
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-		pn_ = SynetPNParser.getPetriNet(new FileInputStream(tmpSaveFile.getAbsolutePath()));
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            if(line.contains("failures") || line.contains("separated")) {
+                if(separationErrorMsg_ == null)
+                    separationErrorMsg_ = "";
+                separationErrorMsg_ += line + "\n";
+            }
+            try {
+                p.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+       
+        pn_ = SynetPNParser.getPetriNet(new FileInputStream(tmpSaveFile.getAbsolutePath()));
+       
+        String errorStr = error.readLine();
+        if (errorStr != null) {
+            errorMsg_ = errorStr;
+            deleteCreatedFiles(tmpAutFile, tmpDisFile, tmpSaveFile);
+            return false;
+        }
+        deleteCreatedFiles(tmpAutFile, tmpDisFile, tmpSaveFile);
+        return true;
+    }
+   
+    /**
+     * Deletes the created files.
+     *
+     * @param tmpSaveFile
+     * @param tmpDisFile
+     * @param tmpAutFile
+     */
+    private void deleteCreatedFiles(File tmpAutFile, File tmpDisFile, File tmpSaveFile) {
+        tmpAutFile.delete();
+        tmpDisFile.delete();
+        tmpSaveFile.delete();
+    }
 
-		String errorStr = error.readLine();
-		if (errorStr != null) {
-			errorMsg_ = errorStr;
-			return false;
-		}
-		return true;
-	}
+    /**
+     * Creates a String with locations out of the Apt-format into Synet-format.
+     *
+     * @return Location String in .dis-format.
+     */
+    private String getDisString() {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> labelMem = new ArrayList<String>(0);
 
-	/**
-	 * Creates a String with locations out of the Apt-format into Synet-format.
-	 *
-	 * @return Location String in .dis-format.
-	 */
-	private String getDisString() {
-		StringBuilder sb = new StringBuilder();
-		ArrayList<String> labelMem = new ArrayList<String>(0);
+        Set<Arc> edges = ts_.getEdges();
+        for (Arc e : edges) {
+            try {
+                if (e.getExtension("location") != null && !labelMem.contains(e.getLabel())) {
+                    sb.append("(" + e.getLabel() + "," + e.getExtension("location").toString().replace("\"", "") + ")");
+                    sb.append("\n");
+                    labelMem.add(e.getLabel());
+                    location_ = true;
+                }
+            } catch (Exception ex) {
+               
+            }
+        }
+        return sb.toString();
+    }
 
-		Set<Arc> edges = ts_.getEdges();
-		for (Arc e : edges) {
-			try {	
-				if (e.getExtension("location") != null && !labelMem.contains(e.getLabel())) {
-					sb.append("(" + e.getLabel() + "," + e.getExtension("location").toString().replace("\"", "") + ")");
-					sb.append("\n");
-					labelMem.add(e.getLabel());
-					location_ = true;
-				}
-			} catch (Exception ex) {
-				
-			}
-		}
-		return sb.toString();
-	}
+    /**
+     * Returns a petrinet which is synthesized out of an location file and an
+     * labeled transition system by Synet.
+     *
+     * @return PetriNet
+     */
+    public PetriNet getPN() {
+        return pn_;
+    }
 
-	/**
-	 * Returns a petrinet which is synthesized out of an location file and an
-	 * labeled transition system by Synet.
-	 *
-	 * @return PetriNet
-	 */
-	public PetriNet getPN() {
-		return pn_;
-	}
-
-	/**
-	 * Error string, created by Synet.
-	 *
-	 * @return String
-	 */
-	public String getError() {
-		return errorMsg_;
-	}
+    /**
+     * Error string, created by Synet.
+     *
+     * @return String
+     */
+    public String getError() {
+        return errorMsg_;
+    }
+   
+    /**
+     * Separation error string, created by Synet.
+     *
+     * @return String
+     */
+    public String getSeparationError() {
+        return separationErrorMsg_;
+    }
 }
 
 // vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
+
