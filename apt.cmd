45f63ab7fbf77662ce81829e5261b2a39460bd86
Dennis Borde <dennis.borde@gmail.com>
2013-08-02 19:08:51 +0200

    Fixed time measurement function in apt.cmd

--- apt.cmd
+++ apt.cmd
@@ -1,15 +1,17 @@
 @echo off
 if not defined TMPFILE ( set TMPFILE="CON" )
 
-set /a timerstart=((1%time:~0,2%-100)*60*60*100)+((1%time:~3,2%-100)*60*100)+((1%time:~6,2%-100)*100)+(1%time:~9,2%-100)
+set /a timerstart = ((1%time:~0,2%-100)*60*60*100)+((1%time:~3,2%-100)*60*100)+((1%time:~6,2%-100)*100)+(1%time:~9,2%-100)
 
 java -Xmx2g -jar apt.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set /a timerstop=((1%time:~0,2%-100)*60*60*100)+((1%time:~3,2%-100)*60*100)+((1%time:~6,2%-100)*100)+(1%time:~9,2%-100)
 
-set /a timemilis=(%timerstop%-%timerstart%)
-set /a timeseks=(%timerstop%-%timerstart%)/100
-set /a timemins=(%timerstop%-%timerstart%)/6000
-
+set /a timediff=%timerstop%-%timerstart%
+set /a timemilis=(%timediff%)%%(100)
+set /a timeseks=(%timediff%/100)%%(60)
+set /a timemins=(%timediff%/6000)%%(60)
+set /a timehours=%timediff%/360000
+              
 echo.
-echo time	%timemins%:%timeseks%.%timemilis% > %TMPFILE%
+echo time	%timehours%:%timemins%:%timeseks%.%timemilis% > %TMPFILE%
