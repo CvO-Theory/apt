6069c493c3b19bcfa0bb127f47789c77fc2c42bf
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2014-11-07 13:53:55 +0100

    Add a spanning tree implementation
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

---
+++ src/uniol/apt/util/SpanningTree.java
@@ -0,0 +1,159 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2014  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.util;
+
+import uniol.apt.adt.IEdge;
+import uniol.apt.adt.IGraph;
+import uniol.apt.adt.INode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Calculate a spanning tree of a graph
+ * @author Uli Schlachter
+ */
+public class SpanningTree<G extends IGraph<G, E, N>, E extends IEdge<G, E, N>, N extends INode<G, E, N>> {
+	private final Map<N, N> predecessorMap;
+	private final Set<N> unreachableNodes;
+	private final N startNode;
+	private final G graph;
+	private final Map<N, List<N>> pathMap = new HashMap<>();
+
+	// TODO: Possible extensions: Spanning forest?
+
+	/**
+	 * Construct a new spanning tree
+	 * @param graph The graph for which a spanning tree should be constructed.
+	 * @param startNode The start node for the spanning tree.
+	 */
+	public SpanningTree(G graph, N startNode) {
+		// Calculate the spanning tree: For each node we remember its predecessor in the tree and we keep a set
+		// of unvisited nodes. We visit unvisited nodes in turn, look at all their children which we haven't
+		// visited yet and enlarge the spanning tree by the path from the current node to the children.
+		Map<N, N> predecessorMap = new HashMap<>();
+		Set<N> unvisitedNodes = new HashSet<>(graph.getNodes());
+		Set<N> stillToVisit = new HashSet<>();
+
+		// Start by visiting the start node.
+		if (startNode != null) {
+			stillToVisit.add(startNode);
+			unvisitedNodes.remove(startNode);
+		}
+
+		while (!stillToVisit.isEmpty()) {
+			N node = stillToVisit.iterator().next();
+			stillToVisit.remove(node);
+
+			// For each child of the current node...
+			for (N child : node.getPostsetNodes()) {
+				// ...if it was not yet reached, mark it as reachable and...
+				if (unvisitedNodes.remove(child)) {
+					// ...remember the path to the start node.
+					predecessorMap.put(child, node);
+					// Also, we have to visit this node later.
+					stillToVisit.add(child);
+				}
+			}
+		}
+
+		this.predecessorMap = Collections.unmodifiableMap(predecessorMap);
+		this.unreachableNodes = Collections.unmodifiableSet(unvisitedNodes);
+		this.startNode = startNode;
+		this.graph = graph;
+	}
+
+	/**
+	 * Construct a new spanning tree
+	 * @param graph The graph for which a spanning tree should be constructed.
+	 */
+	public SpanningTree(G graph) {
+		this(graph, graph.getNodes().isEmpty() ? null : graph.getNodes().iterator().next());
+	}
+
+	/**
+	 * Get all the nodes in the graph which are not reachable from the start node.
+	 */
+	public Set<N> getUnreachableNodes() {
+		return unreachableNodes;
+	}
+
+	/**
+	 * Get the predecessor in the tree.
+	 * @param node The node whose predecessor should be returned.
+	 * @return The predecessor if one exists or null. The start node and unreachable nodes do not have predecessors.
+	 */
+	public N getPredecessor(N node) {
+		return predecessorMap.get(node);
+	}
+
+	/**
+	 * Get the path from the start node.
+	 * @param node The node whose path to return.
+	 * @return The path from the start node to this node. The first entry will be the start node, the last one will
+	 * be the given node.
+	 */
+	public List<N> getPathFromStart(N node) {
+		if (!predecessorMap.containsKey(node) && !node.equals(startNode)) {
+			return Collections.emptyList();
+		}
+
+		// TODO: Optimization: This (indirectly) also calculates the paths for the previous nodes, it would be
+		// nice to save them
+		List<N> result = pathMap.get(node);
+
+		if (result == null) {
+			result = new ArrayList<>();
+
+			while (node != null) {
+				result.add(node);
+				node = getPredecessor(node);
+			}
+
+			Collections.reverse(result);
+			result = Collections.unmodifiableList(result);
+			pathMap.put(node, result);
+		}
+
+		return result;
+	}
+
+	/**
+	 * @return The start node from which the tree was constructed.
+	 */
+	public N getStartNode() {
+		return startNode;
+	}
+
+	/**
+	 * @return The graph from which the tree was constructed.
+	 */
+	public G getGraph() {
+		return graph;
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
---
+++ test/uniol/apt/util/SpanningTreeTest.java
@@ -0,0 +1,159 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2014  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.util;
+
+import uniol.apt.TestTSCollection;
+import uniol.apt.adt.ts.Arc;
+import uniol.apt.adt.ts.State;
+import uniol.apt.adt.ts.TransitionSystem;
+
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/** @author Uli Schlachter */
+@Test
+public class SpanningTreeTest {
+	@Test
+	public void testEmptyTS() {
+		TransitionSystem ts = new TransitionSystem();
+
+		SpanningTree<TransitionSystem, Arc, State> tree = new SpanningTree<>(ts);
+
+		assertThat(tree.getStartNode(), is(equalTo(null)));
+		assertThat(tree.getUnreachableNodes(), is(empty()));
+	}
+
+	private void verifySingleStateTS(SpanningTree<TransitionSystem, Arc, State> tree) {
+		TransitionSystem ts = tree.getGraph();
+		State s0 = ts.getNode("s0");
+
+		assertThat(tree.getStartNode(), is(equalTo(ts.getInitialState())));
+		assertThat(tree.getUnreachableNodes(), is(empty()));
+
+		assertThat(tree.getPredecessor(s0), is(equalTo(null)));
+
+		assertThat(tree.getPathFromStart(s0), contains(s0));
+	}
+
+	@Test
+	public void testSingleStateTS() {
+		TransitionSystem ts = TestTSCollection.getSingleStateTS();
+		verifySingleStateTS(new SpanningTree<>(ts, ts.getInitialState()));
+	}
+
+	@Test
+	public void testSingleStateTS2() {
+		TransitionSystem ts = TestTSCollection.getSingleStateTS();
+		verifySingleStateTS(new SpanningTree<>(ts));
+	}
+
+	@Test
+	public void testcc1LTS() {
+		TransitionSystem ts = TestTSCollection.getcc1LTS();
+		State s0 = ts.getNode("s0");
+		State s1 = ts.getNode("s1");
+		State s2 = ts.getNode("s2");
+		State s3 = ts.getNode("s3");
+
+		SpanningTree<TransitionSystem, Arc, State> tree = new SpanningTree<>(ts, ts.getInitialState());
+
+		assertThat(tree.getStartNode(), is(equalTo(ts.getInitialState())));
+		assertThat(tree.getUnreachableNodes(), is(empty()));
+
+		assertThat(tree.getPredecessor(s0), is(equalTo(null)));
+		assertThat(tree.getPredecessor(s1), is(equalTo(s0)));
+		assertThat(tree.getPredecessor(s2), is(equalTo(s0)));
+		assertThat(tree.getPredecessor(s3), is(anyOf(equalTo(s1), equalTo(s2))));
+
+		assertThat(tree.getPathFromStart(s0), contains(s0));
+		assertThat(tree.getPathFromStart(s1), contains(s0, s1));
+		assertThat(tree.getPathFromStart(s2), contains(s0, s2));
+		assertThat(tree.getPathFromStart(s3), anyOf(contains(s0, s1, s3), contains(s0, s2, s3)));
+	}
+
+	@Test
+	public void testThreeStatesTwoEdgesTS() {
+		TransitionSystem ts = TestTSCollection.getThreeStatesTwoEdgesTS();
+		State s = ts.getNode("s");
+		State t = ts.getNode("t");
+		State v = ts.getNode("v");
+
+		SpanningTree<TransitionSystem, Arc, State> tree = new SpanningTree<>(ts, ts.getInitialState());
+
+		assertThat(tree.getStartNode(), is(equalTo(ts.getInitialState())));
+		assertThat(tree.getUnreachableNodes(), is(empty()));
+
+		assertThat(tree.getPredecessor(s), is(equalTo(null)));
+		assertThat(tree.getPredecessor(t), is(equalTo(s)));
+		assertThat(tree.getPredecessor(v), is(equalTo(s)));
+
+		assertThat(tree.getPathFromStart(s), contains(s));
+		assertThat(tree.getPathFromStart(t), contains(s, t));
+		assertThat(tree.getPathFromStart(v), contains(s, v));
+	}
+
+	@Test
+	public void testPersistentTS() {
+		TransitionSystem ts = TestTSCollection.getPersistentTS();
+		State s0 = ts.getNode("s0");
+		State l = ts.getNode("l");
+		State r = ts.getNode("r");
+		State s1 = ts.getNode("s1");
+
+		SpanningTree<TransitionSystem, Arc, State> tree = new SpanningTree<>(ts, ts.getInitialState());
+
+		assertThat(tree.getStartNode(), is(equalTo(ts.getInitialState())));
+		assertThat(tree.getUnreachableNodes(), is(empty()));
+
+		assertThat(tree.getPredecessor(s0), is(equalTo(null)));
+		assertThat(tree.getPredecessor(l), is(equalTo(s0)));
+		assertThat(tree.getPredecessor(r), is(equalTo(s0)));
+		assertThat(tree.getPredecessor(s1), is(anyOf(equalTo(l), equalTo(r))));
+
+		assertThat(tree.getPathFromStart(s0), contains(s0));
+		assertThat(tree.getPathFromStart(l), contains(s0, l));
+		assertThat(tree.getPathFromStart(r), contains(s0, r));
+		assertThat(tree.getPathFromStart(s1), anyOf(contains(s0, l, s1), contains(s0, r, s1)));
+	}
+
+	@Test
+	public void testNotTotallyReachableTS() {
+		TransitionSystem ts = TestTSCollection.getNotTotallyReachableTS();
+		State s0 = ts.getNode("s0");
+		State s1 = ts.getNode("s1");
+		State fail = ts.getNode("fail");
+
+		SpanningTree<TransitionSystem, Arc, State> tree = new SpanningTree<>(ts, ts.getInitialState());
+
+		assertThat(tree.getStartNode(), is(equalTo(ts.getInitialState())));
+		assertThat(tree.getUnreachableNodes(), contains(fail));
+
+		assertThat(tree.getPredecessor(s0), is(equalTo(null)));
+		assertThat(tree.getPredecessor(s1), is(equalTo(s0)));
+		assertThat(tree.getPredecessor(fail), is(equalTo(null)));
+
+		assertThat(tree.getPathFromStart(s0), contains(s0));
+		assertThat(tree.getPathFromStart(s1), contains(s0, s1));
+		assertThat(tree.getPathFromStart(fail), is(empty()));
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
