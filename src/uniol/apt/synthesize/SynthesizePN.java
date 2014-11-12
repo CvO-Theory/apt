8a5a965d925dd93acc13d8f6b8ee2bf34b806373
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2014-11-12 13:35:30 +0100

    Add class for PetriNet synthesis
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

---
+++ src/uniol/apt/synthesize/SynthesizePN.java
@@ -0,0 +1,257 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2014  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.synthesize;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import uniol.apt.adt.exception.NoSuchNodeException;
+import uniol.apt.adt.exception.NodeExistsException;
+import uniol.apt.adt.pn.PetriNet;
+import uniol.apt.adt.pn.Place;
+import uniol.apt.adt.pn.Transition;
+import uniol.apt.adt.ts.Arc;
+import uniol.apt.adt.ts.State;
+import uniol.apt.adt.ts.TransitionSystem;
+import uniol.apt.util.Pair;
+
+/**
+ * Synthesize a Petri Net from a transition system.
+ * @author Uli Schlachter
+ */
+public class SynthesizePN {
+	private final TransitionSystem ts;
+	private final List<Region> regionBasis = new ArrayList<>();;
+	private final RegionUtility utility;
+	private final Set<Region> regions = new HashSet<>();
+	private final Set<Set<State>> failedStateSeparationProblems = new HashSet<>();
+	private final Set<Pair<String, State>> failedEventStateSeparationProblems = new HashSet<>();
+
+	private static void debug(String message) {
+		//System.err.println("SynthesizePN: " + message);
+	}
+
+	private static void debug() {
+		debug("");
+	}
+
+	private static void debug(Object obj) {
+		debug(obj.toString());
+	}
+
+	/**
+	 * Synthesize a Petri Net which generates the given transition system.
+	 * @param utility An instance of RegionUtility for the requested transition system.
+	 */
+	public SynthesizePN(RegionUtility utility) {
+		this.ts = utility.getTransitionSystem();
+		this.utility = utility;
+
+		for (Region r : new RegionBasis(utility))
+			regionBasis.add(r);
+		debug("Region basis: " + regionBasis);
+
+		// TODO: Test preconditions on ts (deterministic, totally reachable, reduced)
+		// Deterministic: Non-determinism will make SSP fail
+		// Totally Reachable: Uhm. NullPointerExceptions when we try to get a parikh vector? (Or if empty
+		// vector: SSP fails)
+		// reduced: Otherwise the resulting PN will contain an isolated transitions.
+
+		debug();
+		debug("Solving state separation");
+		solveStateSeparation();
+
+		debug();
+		debug("Solving event-state separation");
+		solveEventStateSeparation();
+
+		debug();
+	}
+
+	/**
+	 * Synthesize a Petri Net which generates the given transition system.
+	 * @param ts The transition system to synthesize.
+	 */
+	public SynthesizePN(TransitionSystem ts) {
+		this(new RegionUtility(ts));
+	}
+
+	/**
+	 * Solve all instances of the state separation problem (SSP).
+	 */
+	private void solveStateSeparation() {
+		Set<State> alreadyHandled = new HashSet<>();
+		for (State state : ts.getNodes()) {
+			alreadyHandled.add(state);
+			for (State otherState : ts.getNodes()) {
+				if (alreadyHandled.contains(otherState))
+					continue;
+
+				debug("Trying to separate " + state + " from " + otherState);
+				Region r = SeparationUtility.findSeparatingRegion(utility, regions, state, otherState);
+				if (r == null) {
+					r = SeparationUtility.findSeparatingRegion(utility, regionBasis, state, otherState);
+					if (r == null) {
+						Set<State> problem = new HashSet<>();
+						problem.add(state);
+						problem.add(otherState);
+						failedStateSeparationProblems.add(problem);
+
+						debug("Failure!");
+					} else {
+						debug("Found region " + r);
+						regions.add(r);
+					}
+				} else {
+					debug("Found already-existing separating region " + r);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Solve all instances of the event/state separation problem (ESSP).
+	 */
+	private void solveEventStateSeparation() {
+		for (State state : ts.getNodes())
+			for (String event : ts.getAlphabet()) {
+				//Passende debug()-Aufrufe beibehalten!
+				if (getFollowingState(state, event) == null) {
+					debug("Trying to separate " + state + " from event '" + event + "'");
+					Region r = SeparationUtility.findSeparatingRegion(utility, regions, state, event);
+					if (r == null) {
+						r = SeparationUtility.calculateSeparatingRegion(utility, regionBasis, state, event);
+						if (r == null) {
+							failedEventStateSeparationProblems.add(new Pair<>(event, state));
+							debug("Failure!");
+						} else {
+							debug("Found region " + r);
+							regions.add(r);
+						}
+					} else {
+						debug("Found already-existing separating region " + r);
+					}
+				}
+			}
+	}
+
+	/**
+	 * Get the state which is reached by firing the given event in the given state.
+	 */
+	private State getFollowingState(State state, String event) {
+		for (Arc arc : state.getPostsetEdges())
+			if (arc.getLabel().equals(event))
+				return arc.getTarget();
+		return null;
+	}
+
+	/**
+	 * Get all separating regions which were calculated
+	 */
+	public Set<Region> getSeparatingRegions() {
+		return Collections.unmodifiableSet(regions);
+	}
+
+	/**
+	 * Check if a PetriNet was successfully separated.
+	 */
+	public boolean wasSuccessfullySeparated() {
+		return failedStateSeparationProblems.isEmpty() && failedEventStateSeparationProblems.isEmpty();
+	}
+
+	/**
+	 * Get all the state separation problems which could not be solved.
+	 * @return A set containing sets of two states which cannot be differentiated by any region.
+	 */
+	public Set<Set<State>> getFailedStateSeparationProblems() {
+		return Collections.unmodifiableSet(failedStateSeparationProblems);
+	}
+
+	/**
+	 * Get all the event/state separation problems which could not be solved.
+	 * @return A set containing instances of the event/state separation problem.
+	 */
+	public Set<Pair<String, State>> getFailedEventStateSeparationProblems() {
+		return Collections.unmodifiableSet(failedEventStateSeparationProblems);
+	}
+
+	/**
+	 * Synthesize a Petri Net from the separating regions that were calculated.
+	 */
+	public PetriNet synthesizePetriNet() {
+		if (!wasSuccessfullySeparated())
+			return null;
+		return synthesizePetriNet(regions);
+	}
+
+	/**
+	 * Synthesize a Petri Net from the given regions.
+	 * @param regions The regions that should be used for synthesis.
+	 */
+	public static PetriNet synthesizePetriNet(Set<Region> regions) {
+		PetriNet pn = new PetriNet();
+
+		debug("Synthesizing PetriNet from these regions:");
+		debug(regions);
+
+		for (Region region : regions) {
+			Place place = pn.createPlace();
+			place.setInitialToken(region.getNormalRegionMarking());
+			place.putExtension(Region.class.getName(), region);
+
+			for (String event : region.getRegionUtility().getEventList()) {
+				Transition transition = getOrCreateTransition(pn, event);
+				int backward = region.getBackwardWeight(event);
+				assert backward >= 0;
+				if (backward > 0)
+					pn.createFlow(place, transition, backward);
+
+				int forward = region.getForwardWeight(event);
+				assert forward >= 0;
+				if (forward > 0)
+					pn.createFlow(transition, place, forward);
+			}
+		}
+
+		return pn;
+	}
+
+	/**
+	 * Get or create the given transition in the Petri Net.
+	 */
+	private static Transition getOrCreateTransition(PetriNet pn, String id) {
+		try {
+			return pn.getTransition(id);
+		} catch (NoSuchNodeException ex) {
+			try {
+				return pn.createTransition(id);
+			} catch (NodeExistsException ex2) {
+				throw new AssertionError("Tried to get or create transition with id '" + id + "'. " +
+						"Getting failed, claiming that the transition doesn't exist. " +
+						"Creating failed, claiming that the transition already exists.", ex2);
+			}
+		}
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
---
+++ test/uniol/apt/synthesize/SynthesizePNTest.java
@@ -0,0 +1,139 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2014  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.synthesize;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import uniol.apt.TestTSCollection;
+import uniol.apt.adt.pn.PetriNet;
+import uniol.apt.adt.pn.Place;
+import uniol.apt.adt.ts.TransitionSystem;
+import uniol.apt.adt.ts.State;
+import uniol.apt.util.Pair;
+
+import org.hamcrest.collection.IsIterableWithSize;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static uniol.apt.adt.matcher.Matchers.flowThatConnects;
+import static uniol.apt.adt.matcher.Matchers.nodeWithID;
+import static uniol.apt.synthesize.Matchers.*;
+
+/** @author Uli Schlachter */
+@Test
+@SuppressWarnings("unchecked") // I hate generics
+public class SynthesizePNTest {
+	static private Region mockRegion(RegionUtility utility, int normalMarking, List<Integer> backwardWeights, List<Integer> forwardWeights)
+	{
+		assert backwardWeights.size() == forwardWeights.size();
+
+		Region result = mock(Region.class);
+		when(result.getRegionUtility()).thenReturn(utility);
+		when(result.getNormalRegionMarking()).thenReturn(normalMarking);
+
+		List<String> eventList = utility.getEventList();
+		for (int i = 0; i < backwardWeights.size(); i++) {
+			when(result.getBackwardWeight(i)).thenReturn(backwardWeights.get(i));
+			when(result.getBackwardWeight(eventList.get(i))).thenReturn(backwardWeights.get(i));
+			when(result.getForwardWeight(i)).thenReturn(forwardWeights.get(i));
+			when(result.getForwardWeight(eventList.get(i))).thenReturn(forwardWeights.get(i));
+		}
+
+		return result;
+	}
+
+	@Test
+	public void testSynthesizePetriNetEmpty() {
+		PetriNet pn = SynthesizePN.synthesizePetriNet(Collections.<Region>emptySet());
+
+		assertThat(pn.getPlaces(), is(empty()));
+		assertThat(pn.getTransitions(), is(empty()));
+		assertThat(pn.getEdges(), is(empty()));
+	}
+
+	@Test
+	public void testSynthesizeSimplePetriNet() {
+		List<String> eventList = Arrays.asList("a", "b");
+		RegionUtility utility = mock(RegionUtility.class);
+		when(utility.getEventList()).thenReturn(eventList);
+
+		Set<Region> regions = new HashSet<>();
+		regions.add(mockRegion(utility, 1, Arrays.asList(1, 1), Arrays.asList(0, 0)));
+
+		PetriNet pn = SynthesizePN.synthesizePetriNet(regions);
+
+		assertThat(pn.getPlaces(), IsIterableWithSize.<Place>iterableWithSize(1));
+		assertThat(pn.getTransitions(), containsInAnyOrder(nodeWithID("a"), nodeWithID("b")));
+		assertThat(pn.getEdges(), containsInAnyOrder(
+					flowThatConnects(anything(), nodeWithID("a")),
+					flowThatConnects(anything(), nodeWithID("b"))));
+	}
+
+	@Test
+	public void testNonDeterministicTS() {
+		TransitionSystem ts = TestTSCollection.getNonDeterministicTS();
+		RegionUtility utility = new RegionUtility(ts);
+		SynthesizePN synth = new SynthesizePN(utility);
+
+		assertThat(synth.wasSuccessfullySeparated(), is(false));
+		// Can't really be more specific, way too many possibilities (TODO: Optimize the code so this becomes
+		// testable)
+		assertThat(synth.getSeparatingRegions(), not(empty()));
+		assertThat(synth.getFailedStateSeparationProblems(), contains(containsInAnyOrder(nodeWithID("s1"), nodeWithID("s2"))));
+		assertThat(synth.getFailedEventStateSeparationProblems(), empty());
+	}
+
+	@Test
+	public void testPathTS() {
+		TransitionSystem ts = TestTSCollection.getPathTS();
+		RegionUtility utility = new RegionUtility(ts);
+		SynthesizePN synth = new SynthesizePN(utility);
+
+		assertThat(synth.wasSuccessfullySeparated(), is(false));
+		// Can't really be more specific, way too many possibilities
+		assertThat(synth.getSeparatingRegions(), not(empty()));
+		assertThat(synth.getFailedStateSeparationProblems(), contains(containsInAnyOrder(nodeWithID("t"), nodeWithID("u"))));
+		assertThat(synth.getFailedEventStateSeparationProblems(), containsInAnyOrder(
+					equalTo(new Pair<String, State>("c", ts.getNode("t"))),
+					equalTo(new Pair<String, State>("b", ts.getNode("v"))),
+					equalTo(new Pair<String, State>("b", ts.getNode("u"))),
+					equalTo(new Pair<String, State>("b", ts.getNode("s")))));
+	}
+
+	@Test
+	public void testPureSynthesizablePathTS() {
+		TransitionSystem ts = TestTSCollection.getPureSynthesizablePathTS();
+		RegionUtility utility = new RegionUtility(ts);
+		SynthesizePN synth = new SynthesizePN(utility);
+
+		assertThat(synth.wasSuccessfullySeparated(), is(true));
+		// Can't really be more specific, way too many possibilities
+		assertThat(synth.getSeparatingRegions(), IsIterableWithSize.<Region>iterableWithSize(greaterThanOrEqualTo(3)));
+		assertThat(synth.getFailedStateSeparationProblems(), empty());
+		assertThat(synth.getFailedEventStateSeparationProblems(), empty());
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
