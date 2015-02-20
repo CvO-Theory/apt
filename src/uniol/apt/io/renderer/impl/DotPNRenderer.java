317c846f7a2bed485baf64bfcf79ceb33db4da16
vsp <vsp@chuck.Informatik.Uni-Oldenburg.DE>
2015-02-20 21:43:38 +0100

    DotPNRenderer: new implementation which uses stringtemplate
    Generated Outputs contain the xlabel node option, which needs at least graphviz
    version 2.29.
    
    Signed-off-by: vsp <vsp@chuck.Informatik.Uni-Oldenburg.DE>

--- src/uniol/apt/io/renderer/impl/DotPNRenderer.java
+++ src/uniol/apt/io/renderer/impl/DotPNRenderer.java
@@ -1,6 +1,6 @@
 /*-
  * APT - Analysis of Petri Nets and labeled Transition systems
- * Copyright (C) 2012-2013  Members of the project group APT
+ * Copyright (C) 2015       vsp
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,84 +21,29 @@ package uniol.apt.io.renderer.impl;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Formatter;
 
-import uniol.apt.adt.pn.Flow;
+import org.stringtemplate.v4.AutoIndentWriter;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
+
 import uniol.apt.adt.pn.PetriNet;
-import uniol.apt.adt.pn.Place;
-import uniol.apt.adt.pn.Token;
-import uniol.apt.adt.pn.Transition;
 import uniol.apt.io.renderer.Renderer;
 import uniol.apt.io.renderer.RenderException;
 
 /**
- * This class converts Petri nets and transition systems to dot file strings.
- *
- *
- * @author Renke Grunwald
- *
+ * This class renders Petri nets in the dot graph file format
+ * @author vsp
  */
 public class DotPNRenderer extends AbstractRenderer<PetriNet> implements Renderer<PetriNet> {
-	private final static String PN_PLACE_TEMPLATE =
-		"%1$s[label=\"%2$s\"]\n%1$s_label[shape=plaintext,label=\"\"]\n%1$s -> %1$s_label[penwidth=0,"
-		+ "label=\"%1$s\",arrowhead=none]\n";
-	private final static String PN_TRANSITION_TEMPLATE =
-		"%1$s[label=\"%2$s\"]\n%1$s_label[shape=plaintext,label=\"\"]\n%1$s -> %1$s_label[penwidth=0,"
-		+ "label=\"%1$s\",arrowhead=none]\n";
-	private final static String PN_FLOW_TEMPLATE = "%1$s -> %2$s[label=\"%3$s\"]\n";
-
 	@Override
 	public void render(PetriNet pn, Writer writer) throws RenderException, IOException {
-		// TODO: Use external labels (from recent Graphviz versions)
-		writer.append("digraph G {\n");
-		writer.append("edge [fontsize=20]\n");
+		STGroup group = new STGroupFile("uniol/apt/io/renderer/impl/DotPN.stg");
+		ST pnTemplate = group.getInstanceOf("pn");
 
-		//t1[label="t1"]
-		//t1_label[shape=plaintext,label=""]
-		//t1 -> t1_label[penwidth=0,label="t1",arrowhead=none]
+		pnTemplate.add("pn", pn);
 
-		writer.append("node [fontsize=20,shape=rect,height=0.5,width=0.5,fixedsize=true];\n");
-		Formatter transitionFormat = new Formatter(writer);
-
-		for (Transition transition : pn.getTransitions()) {
-			transitionFormat.format(PN_TRANSITION_TEMPLATE, transition.getId(), transition.getLabel());
-		}
-
-		transitionFormat.close();
-
-		writer.append("node [fontsize=20,shape=circle,height=0.5,width=0.5,fixedsize=true];\n");
-		Formatter placeFormat = new Formatter(writer);
-
-		for (Place place : pn.getPlaces()) {
-			Token tokens = pn.getInitialMarking().getToken(place);
-			String tokensString = "";
-
-			if (tokens.getValue() > 0) {
-				tokensString = "" + tokens.getValue();
-			}
-
-			placeFormat.format(PN_PLACE_TEMPLATE, place.getId(), tokensString);
-		}
-
-		placeFormat.close();
-
-		Formatter flowFormat = new Formatter(writer);
-
-		for (Flow arc : pn.getEdges()) {
-			if (arc.getWeight() >= 1) {
-				String weightString = " ";
-
-				if (arc.getWeight() > 1) {
-					weightString = "" + arc.getWeight();
-				}
-
-				flowFormat.format(PN_FLOW_TEMPLATE, arc.getSource().getId(), arc.getTarget().getId(),
-						"" + weightString);
-			}
-		}
-
-		flowFormat.close();
-		writer.append("}\n");
+		pnTemplate.write(new AutoIndentWriter(writer), new ThrowingErrorListener());
 	}
 }
 
