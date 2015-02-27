2d4767096506a8ca7bd307918f181fdc1dec4c54
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2015-02-27 13:30:40 +0100

    Add an implementation of an EquivalenceRelation
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

---
+++ src/uniol/apt/util/EquivalenceRelation.java
@@ -0,0 +1,134 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2015  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.util;
+
+import java.util.Set;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.Iterator;
+
+import org.apache.commons.collections4.map.LazyMap;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.iterators.PeekingIterator;
+import static org.apache.commons.collections4.iterators.PeekingIterator.peekingIterator;
+
+/**
+ * Instances of this class represent an equivalence relation over an unknown set. Initially, all elements are only
+ * equivalent to themselves, but the equivalence classes of two elements can be joined.
+ * @author Uli Schlachter
+ */
+public class EquivalenceRelation<E> implements Iterable<Set<E>> {
+	private class ToUnitSetTransformer implements Transformer<E, Set<E>> {
+		@Override
+		public Set<E> transform(E e) {
+			Set<E> result = new HashSet<>();
+			result.add(e);
+			EquivalenceRelation.this.allClasses.add(result);
+			return result;
+		}
+	}
+
+	private final Map<E, Set<E>> elementToClass = LazyMap.lazyMap(new HashMap<E, Set<E>>(), new ToUnitSetTransformer());
+	private final Set<Set<E>> allClasses = new HashSet<>();
+
+	/**
+	 * Join the equivalence classes of two elements.
+	 * @param e1 The first element to join classes with
+	 * @param e2 The other element to join classes with
+	 * @return the new class containing both elements
+	 */
+	public Set<E> joinClasses(E e1, E e2) {
+		Set<E> class1 = elementToClass.get(e1);
+		Set<E> class2 = elementToClass.get(e2);
+
+		if (class1.contains(e2))
+			// Already in same class
+			return class1;
+
+		// Make class1 refer to the smaller of the two classes.
+		if (class1.size() > class2.size()) {
+			Set<E> tmp = class1;
+			class1 = class2;
+			class2 = tmp;
+		}
+
+		allClasses.remove(class1);
+		allClasses.remove(class2);
+		class2.addAll(class1);
+		allClasses.add(class2);
+
+		for (E e : class1)
+			elementToClass.put(e, class2);
+
+		return class2;
+	}
+
+	/**
+	 * Get the equivalence class of the given element.
+	 * @param e the element whose class is needed
+	 * @return The element's equivalence class
+	 */
+	public Set<E> getClass(E e) {
+		return elementToClass.get(e);
+	}
+
+	/**
+	 * Check if the given elements are in the same equivalence class.
+	 * @param e1 The first element to check
+	 * @param e2 The other element to check
+	 * @return true if both elements are equivalent
+	 */
+	public boolean isEquivalent(E e1, E e2) {
+		return elementToClass.get(e1).contains(e2);
+	}
+
+	@Override
+	public Iterator<Set<E>> iterator() {
+		return new Iterator<Set<E>>() {
+			private PeekingIterator<Set<E>> iter = peekingIterator(allClasses.iterator());
+
+			@Override
+			public boolean hasNext() {
+				// Skip all sets which have just a single entry
+				Set<E> next = iter.peek();
+				while (next != null && next.size() == 1) {
+					iter.next();
+					next = iter.peek();
+				}
+				return iter.hasNext();
+			}
+
+			@Override
+			public Set<E> next() {
+				hasNext();
+				return Collections.unmodifiableSet(iter.next());
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
---
+++ test/uniol/apt/util/EquivalenceRelationTest.java
@@ -0,0 +1,109 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2015  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.util;
+
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static uniol.apt.adt.matcher.Matchers.*;
+
+/** @author Uli Schlachter */
+@Test
+@SuppressWarnings("unchecked") // I hate generics
+public class EquivalenceRelationTest {
+	@Test
+	public void testEmptyRelation() {
+		EquivalenceRelation<String> relation = new EquivalenceRelation<>();
+
+		assertThat(relation, emptyIterable());
+		assertThat(relation.isEquivalent("a", "b"), is(false));
+		assertThat(relation.getClass("a"), contains("a"));
+	}
+
+	@Test
+	public void testIterateAfterChecks() {
+		EquivalenceRelation<String> relation = new EquivalenceRelation<>();
+
+		assertThat(relation.isEquivalent("a", "b"), is(false));
+		assertThat(relation.isEquivalent("c", "b"), is(false));
+		assertThat(relation.getClass("a"), contains("a"));
+		assertThat(relation.getClass("c"), contains("c"));
+
+		assertThat(relation, emptyIterable());
+	}
+
+	@Test
+	public void testReflexive() {
+		EquivalenceRelation<String> relation = new EquivalenceRelation<>();
+
+		assertThat(relation.isEquivalent("a", "a"), is(true));
+	}
+
+	@Test
+	public void testTwoElements() {
+		EquivalenceRelation<String> relation = new EquivalenceRelation<>();
+		assertThat(relation.joinClasses("a", "b"), containsInAnyOrder("a", "b"));
+
+		assertThat(relation.isEquivalent("a", "b"), is(true));
+		assertThat(relation.isEquivalent("b", "a"), is(true));
+		assertThat(relation.isEquivalent("c", "b"), is(false));
+		assertThat(relation.getClass("a"), containsInAnyOrder("a", "b"));
+		assertThat(relation.getClass("c"), contains("c"));
+		assertThat(relation.getClass("b"), sameInstance(relation.getClass("a")));
+		assertThat(relation, contains(containsInAnyOrder("a", "b")));
+	}
+
+	@Test
+	public void testThreeElements() {
+		EquivalenceRelation<String> relation = new EquivalenceRelation<>();
+		assertThat(relation.joinClasses("a", "b"), containsInAnyOrder("a", "b"));
+		assertThat(relation.joinClasses("a", "c"), containsInAnyOrder("a", "b", "c"));
+
+		assertThat(relation.isEquivalent("c", "b"), is(true));
+		assertThat(relation.getClass("a"), containsInAnyOrder("a", "b", "c"));
+		assertThat(relation, contains(containsInAnyOrder("a", "b", "c")));
+	}
+
+	@Test
+	public void testTwoClasses() {
+		EquivalenceRelation<String> relation = new EquivalenceRelation<>();
+		assertThat(relation.joinClasses("a", "b"), containsInAnyOrder("a", "b"));
+		assertThat(relation.joinClasses("c", "d"), containsInAnyOrder("c", "d"));
+
+		assertThat(relation.isEquivalent("c", "b"), is(false));
+		assertThat(relation.getClass("a"), containsInAnyOrder("a", "b"));
+		assertThat(relation, containsInAnyOrder(
+					containsInAnyOrder("a", "b"),
+					containsInAnyOrder("c", "d")));
+	}
+
+	@Test
+	public void testFourElements() {
+		EquivalenceRelation<String> relation = new EquivalenceRelation<>();
+		assertThat(relation.joinClasses("a", "b"), containsInAnyOrder("a", "b"));
+		assertThat(relation.joinClasses("c", "d"), containsInAnyOrder("c", "d"));
+		assertThat(relation.joinClasses("a", "d"), containsInAnyOrder("a", "b", "c", "d"));
+
+		assertThat(relation.isEquivalent("c", "b"), is(true));
+		assertThat(relation.getClass("a"), containsInAnyOrder("a", "b", "c", "d"));
+		assertThat(relation, contains(containsInAnyOrder("a", "b", "c", "d")));
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
