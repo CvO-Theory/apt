e29294d78d12dfb5c32a6a3bf05156babdcdffd0
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2015-03-18 14:22:07 +0100

    Add an implementation of a power set
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

---
+++ src/uniol/apt/util/PowerSet.java
@@ -0,0 +1,148 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2015  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.util;
+
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Calculate the power set of a collection.
+ * @author Uli Schlachter
+ */
+public class PowerSet<E> extends AbstractCollection<Collection<E>> {
+	final private List<E> c;
+
+	/**
+	 * Constructor
+	 * @param c The collection whose power set should be generated. This collection must have stable iteration
+	 * order so that iterating multiple times over it always produces elements in the same order!
+	 */
+	public PowerSet(List<E> c) {
+		this.c = c;
+	}
+
+	@Override
+	public int size() {
+		return (int) Math.pow(2, c.size());
+	}
+
+	@Override
+	public Iterator<Collection<E>> iterator() {
+		return new PowerSetIterator<E>(c);
+	}
+
+	/**
+	 * Iterator that produces the power set of a given list.
+	 * @author Uli Schlachter
+	 */
+	static public class PowerSetIterator<E> implements Iterator<Collection<E>> {
+		final private List<E> c;
+		final private Iterator<BitSet> iter;
+
+		/**
+		 * Constructor
+		 * @param c The list whose power set should be generated
+		 */
+		public PowerSetIterator(List<E> c) {
+			this.c = c;
+			this.iter = new BitSetIterator(c.size());
+		}
+
+		@Override
+		public boolean hasNext() {
+			return iter.hasNext();
+		}
+
+		@Override
+		public Collection<E> next() {
+			BitSet next = iter.next();
+			Collection<E> result = new ArrayList<>(next.cardinality());
+			int i = 0;
+			for (E e : c)
+				if (next.get(i++))
+					result.add(e);
+
+			return result;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	/**
+	 * Iterator that iterates through all possible BitSets of a given length.
+	 * @author Uli Schlachter
+	 */
+	static public class BitSetIterator implements Iterator<BitSet> {
+		final private int size;
+		private BitSet state;
+
+		/**
+		 * Constructor
+		 * @param size The number of bits that should be in each BitSet
+		 */
+		public BitSetIterator(int size) {
+			if (size < 0)
+				throw new IllegalArgumentException("Size may not be negative, got " + size);
+			this.size = size;
+			this.state = new BitSet(size);
+		}
+
+		@Override
+		public boolean hasNext() {
+			return state != null;
+		}
+
+		@Override
+		public BitSet next() {
+			BitSet result = state.get(0, size);
+
+			int lastClear = state.previousClearBit(size - 1);
+			if (lastClear >= 0)
+				state.flip(lastClear, size);
+			else
+				state = null;
+
+			return result;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	/**
+	 * Produce the power set of a collection
+	 * @param c The collection whose power set should be generated
+	 * @return An iterable containing the power set of the given list
+	 */
+	static public <E> PowerSet<E> powerSet(List<E> c) {
+		return new PowerSet<E>(c);
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
---
+++ test/uniol/apt/util/PowerSetTest.java
@@ -0,0 +1,112 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2015  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.util;
+
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import static java.util.Arrays.asList;
+
+import org.testng.annotations.Test;
+import org.hamcrest.Matcher;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static uniol.apt.adt.matcher.Matchers.*;
+
+/** @author Uli Schlachter */
+@Test
+@SuppressWarnings("unchecked") // I hate generics
+public class PowerSetTest {
+	static private final Matcher<Collection<? extends Integer>> empty = empty();
+
+	@Test(expectedExceptions = { IllegalArgumentException.class })
+	public void testBitSetNegativeSize() {
+		new PowerSet.BitSetIterator(-42);
+	}
+
+	@Test
+	public void testBitSetZero() {
+		Iterator<BitSet> iter = new PowerSet.BitSetIterator(0);
+
+		assertThat(iter.hasNext(), is(true));
+		BitSet element = iter.next();
+		assertThat(element, equalTo(new BitSet(0)));
+
+		assertThat(iter.hasNext(), is(false));
+	}
+
+	@Test
+	public void testBitSetOne() {
+		Iterator<BitSet> iter = new PowerSet.BitSetIterator(1);
+
+		assertThat(iter.hasNext(), is(true));
+		BitSet element = iter.next();
+		assertThat(element, equalTo(new BitSet(1)));
+
+		assertThat(iter.hasNext(), is(true));
+		element = iter.next();
+		BitSet expected = new BitSet(1);
+		expected.set(0);
+		assertThat(element, equalTo(expected));
+
+		assertThat(iter.hasNext(), is(false));
+	}
+
+	@Test
+	public void testBitSetThree() {
+		Iterator<BitSet> iter = new PowerSet.BitSetIterator(3);
+		Set<BitSet> set = new HashSet<>();
+
+		int numElements = 0;
+		while (iter.hasNext()) {
+			set.add(iter.next());
+			numElements++;
+		}
+
+		assertThat(numElements, is(8));
+		// Cheap check that all elements are really different:
+		assertThat(set, hasSize(8));
+	}
+
+	@Test
+	public void testEmptyList() {
+		PowerSet<Integer> power = PowerSet.powerSet(Collections.<Integer>emptyList());
+		assertThat(power, hasSize(1));
+		assertThat(power, contains(empty()));
+	}
+
+	@Test
+	public void testOneElementList() {
+		PowerSet<Integer> power = PowerSet.powerSet(asList(0));
+		assertThat(power, hasSize(2));
+		assertThat(power, containsInAnyOrder(contains(0), empty));
+	}
+
+	@Test
+	public void testTwoElementList() {
+		PowerSet<Integer> power = PowerSet.powerSet(asList(0, 1));
+		assertThat(power, hasSize(4));
+		assertThat(power, containsInAnyOrder(contains(0, 1), contains(0), contains(1), empty));
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
