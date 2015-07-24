9c902366d0db5af0e247d190eeb758540e5f20c8
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2015-07-24 16:40:05 +0200

    Factor out a helper class from InequalitySystemSeparation
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

--- src/uniol/apt/analysis/synthesize/separation/InequalitySystemSeparation.java
+++ src/uniol/apt/analysis/synthesize/separation/InequalitySystemSeparation.java
@@ -1,6 +1,6 @@
 /*-
  * APT - Analysis of Petri Nets and labeled Transition systems
- * Copyright (C) 2014  Uli Schlachter
+ * Copyright (C) 2014-2015  Uli Schlachter
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -55,27 +55,14 @@ import static uniol.apt.util.DebugUtil.debug;
  * @author Uli Schlachter
  */
 class InequalitySystemSeparation implements Separation {
+	private final SMTInterpolHelper helper;
+	private final Script script;
 	private final RegionUtility utility;
 	private final PNProperties properties;
-	private final String[] locationMap;
-	private final Script script = Log4JInitializationHelper.INSTANCE.createScript();
 	private final Term regionInitialMarking;
 	private final Term[] regionWeights;
 	private final Term[] regionBackwardWeights;
 	private final Term[] regionForwardWeights;
-
-	private static class Log4JInitializationHelper {
-		public static final Log4JInitializationHelper INSTANCE = new Log4JInitializationHelper();
-
-		private Log4JInitializationHelper() {
-			// Set up SMTInterpol in a way that it doesn't produce debug output
-			Logger.getRootLogger().addAppender(new NullAppender());
-		}
-
-		public Script createScript() {
-			return new SMTInterpol(Logger.getRootLogger(), false);
-		}
-	}
 
 	/**
 	 * Construct a new instance for solving separation problems.
@@ -88,10 +75,8 @@ class InequalitySystemSeparation implements Separation {
 		final List<String> eventList = utility.getEventList();
 		this.utility = utility;
 		this.properties = new PNProperties(properties);
-		this.locationMap = locationMap;
-		script.setLogic(Logics.QF_LIA);
-
-		defineIsRegion();
+		this.helper = new SMTInterpolHelper(utility, properties, locationMap);
+		this.script = helper.getScript();
 
 		// Finally, we define the needed variables
 		Sort[] emptySort = new Sort[0];
@@ -127,298 +112,6 @@ class InequalitySystemSeparation implements Separation {
 			}
 		}
 		script.assertTerm(script.term("isRegion", params));
-	}
-
-	/**
-	 * Define the isRegion function in the script.
-	 */
-	private void defineIsRegion() {
-		final int numberEvents = utility.getNumberOfEvents();
-		final List<String> eventList = utility.getEventList();
-
-		TermVariable[] params;
-		TermVariable initialMarking;
-		TermVariable[] weight = new TermVariable[numberEvents];
-		TermVariable[] backwardWeight = new TermVariable[numberEvents];
-		TermVariable[] forwardWeight = new TermVariable[numberEvents];
-		TermVariable[] letVariables;
-		Term[] letTerms;
-
-		for (int event = 0; event < numberEvents; event++) {
-			weight[event] = script.variable("e-" + eventList.get(event), script.sort("Int"));
-			backwardWeight[event] = script.variable("b-" + eventList.get(event), script.sort("Int"));
-			forwardWeight[event] = script.variable("f-" + eventList.get(event), script.sort("Int"));
-		}
-
-		if (properties.isPure()) {
-			params = new TermVariable[1 + numberEvents];
-			letVariables = new TermVariable[2 * numberEvents];
-			letTerms = new Term[2 * numberEvents];
-
-			Term zero = script.numeral(BigInteger.ZERO);
-			for (int event = 0; event < numberEvents; event++) {
-				params[1 + event] = weight[event];
-
-				letVariables[event] = backwardWeight[event];
-				letVariables[event + numberEvents] = forwardWeight[event];
-
-				// If the weight is negative, then backwardWeight = -weight, else backwardWeight = 0
-				letTerms[event] =
-					script.term("ite", script.term(">", zero, weight[event]),
-							script.term("-", weight[event]), zero);
-				// If the weight is positive, then forwardWeight = weight, else forwardWeight = 0
-				letTerms[event + numberEvents] =
-					script.term("ite", script.term("<", zero, weight[event]),
-							weight[event], zero);
-			}
-		} else {
-			params = new TermVariable[1 + 2 * numberEvents];
-			letTerms = new Term[numberEvents];
-			letVariables = weight;
-			for (int event = 0; event < numberEvents; event++) {
-				params[1 + event] = backwardWeight[event];
-				params[1 + event + numberEvents] = forwardWeight[event];
-				letTerms[event] = script.term("-", forwardWeight[event], backwardWeight[event]);
-			}
-		}
-		initialMarking = params[0] = script.variable("m0", script.sort("Int"));
-
-		List<Term> isRegion = new ArrayList<>();
-		isRegion.addAll(requireRegion(initialMarking, weight, backwardWeight, forwardWeight));
-
-		if (properties.isKBounded())
-			isRegion.addAll(requireKBounded(initialMarking, weight, properties.getKForKBounded()));
-
-		// Our definition of conflict-free requires plainness
-		if (properties.isPlain() || properties.isConflictFree())
-			isRegion.addAll(requirePlainness(initialMarking, backwardWeight, forwardWeight));
-		
-		// ON is handled in SeparationUtility by messing with the locationMap
-		assert !properties.isOutputNonbranching();
-
-		isRegion.addAll(requireDistributableNet(backwardWeight));
-		if (properties.isConflictFree())
-			isRegion.addAll(requireConflictFree(weight, backwardWeight));
-		if (properties.isTNet()) {
-			isRegion.addAll(requireTNet(backwardWeight));
-			isRegion.addAll(requireTNet(forwardWeight));
-		}
-
-		// Now we can define the "isRegion" function
-		Term isRegionTerm = script.term("and", isRegion.toArray(new Term[isRegion.size()]));
-		isRegionTerm = script.let(letVariables, letTerms, isRegionTerm);
-		script.defineFun("isRegion", params, script.sort("Bool"), isRegionTerm);
-	}
-
-	/**
-	 * Get a term describing the marking of the given state.
-	 * @param initialMarking A term for the initial marking of the region.
-	 * @param weight An array of terms describing the effective weights of transitions.
-	 * @param state The state whose marking should be calculated.
-	 * @return A term describing the marking of the given state.
-	 * @throws UnreachableException if the given state is unreachable.
-	 */
-	private Term evaluateReachingParikhVector(Term initialMarking, Term[] weight, State state) throws UnreachableException {
-		Term result = evaluateParikhVector(weight,
-				utility.getReachingParikhVector(state));
-		return script.term("+", initialMarking, result);
-	}
-
-	/**
-	 * Get a term describing the effect of the given Parikh vector.
-	 * @param weight An array of terms describing the effective weights of transitions.
-	 * @param pv The Parikh vector to evaluate.
-	 * @return A term describing the effect of the vector.
-	 */
-	private Term evaluateParikhVector(Term[] weight, List<Integer> pv) {
-		assert weight.length == pv.size();
-		Term[] summands = new Term[weight.length];
-		for (int event = 0; event < weight.length; event++) {
-			Term addend;
-			int w = pv.get(event);
-			if (w >= 0)
-				addend = script.numeral(BigInteger.valueOf(w));
-			else
-				addend = script.term("-", script.numeral(BigInteger.valueOf(w).negate()));
-			summands[event] = script.term("*", addend, weight[event]);
-		}
-		return collectTerms("+", summands, script.numeral(BigInteger.ZERO));
-	}
-
-	/**
-	 * Get a list of terms that is required to describe a region.
-	 * @param initialMarking A term representing the initial marking of the region.
-	 * @param weight Terms representing the effective weights of transitions.
-	 * @param backwardWeight Terms representing the backward weights of transitions.
-	 * @param forwardWeight Terms representing the forward weights of transitions.
-	 * @return A list of terms all together describing a region.
-	 * @return An inequality system prepared for calculating separating regions.
-	 */
-	private List<Term> requireRegion(Term initialMarking, Term[] weight, Term[] backwardWeight, Term[] forwardWeight) {
-		List<Term> result = new ArrayList<>();
-		final int events = utility.getNumberOfEvents();
-		Term zero = script.numeral(BigInteger.ZERO);
-
-		// Cycles must reach the same marking again
-		for (Arc chord : utility.getSpanningTree().getChords()) {
-			try {
-				List<Integer> pv = utility.getParikhVectorForEdge(chord);
-				Term term = evaluateParikhVector(weight, pv);
-				term = script.term("=", zero, term);
-				result.add(term);
-			} catch (UnreachableException e) {
-				throw new RuntimeException("Chords of a spanning tree cannot belong to "
-						+ "unreachable states?!", e);
-			}
-		}
-
-		// Each arc must be enabled
-		for (Arc arc : utility.getTransitionSystem().getEdges()) {
-			try {
-				int event = utility.getEventIndex(arc.getLabel());
-				Term term = evaluateReachingParikhVector(initialMarking, weight, arc.getSource());
-				result.add(script.term("<=", backwardWeight[event], term));
-			} catch (UnreachableException e) {
-				// Just ignore unreachable arcs
-				continue;
-			}
-		}
-
-		// Some terms must not be zero
-		result.add(script.term("<=", zero, initialMarking));
-		for (Term term : backwardWeight)
-			result.add(script.term("<=", zero, term));
-		for (Term term : forwardWeight)
-			result.add(script.term("<=", zero, term));
-
-		return result;
-	}
-
-	/**
-	 * Add the needed inequalities so that the system may only produce k-bounded regions.
-	 * @param initialMarking A term representing the initial marking of the region.
-	 * @param weight Terms representing the effective weights of transitions.
-	 * @param k The limit for the bound.
-	 * @return The needed terms.
-	 */
-	private List<Term> requireKBounded(Term initialMarking, Term[] weight, int k) {
-		List<Term> result = new ArrayList<>();
-		Term K = script.numeral(BigInteger.valueOf(k));
-		for (State state : utility.getTransitionSystem().getNodes()) {
-			try {
-				Term term = evaluateReachingParikhVector(initialMarking, weight, state);
-				result.add(script.term("<=", term, K));
-			} catch (UnreachableException e) {
-				continue;
-			}
-		}
-		return result;
-	}
-
-	/**
-	 * Add the needed inequalities so that the system may only produce plain regions.
-	 * @param initialMarking A term representing the initial marking of the region.
-	 * @param backwardWeight Terms representing the backward weights of transitions.
-	 * @param forwardWeight Terms representing the forward weights of transitions.
-	 * @return The needed terms.
-	 */
-	private List<Term> requirePlainness(Term initialMarking, Term[] backwardWeight, Term[] forwardWeight) {
-		List<Term> result = new ArrayList<>();
-		Term one = script.numeral(BigInteger.ONE);
-		for (int event = 0; event < utility.getNumberOfEvents(); event++) {
-			result.add(script.term(">=", one, backwardWeight[event]));
-			result.add(script.term(">=", one, forwardWeight[event]));
-		}
-		return result;
-	}
-
-	/**
-	 * Add the needed inequalities so that the system may only produce T-Net regions for the given weights. This
-	 * must be called for both the presets and postsets.
-	 * @param weight Terms representing the weights of transitions.
-	 * @return The needed terms.
-	 */
-	private List<Term> requireTNet(Term[] weight) {
-		final int numberEvents = utility.getNumberOfEvents();
-		Term[] result = new Term[numberEvents];
-		Term zero = script.numeral(BigInteger.ZERO);
-
-		for (int event = 0; event < numberEvents; event++) {
-			Term[] sum = new Term[utility.getNumberOfEvents() - 1];
-			for (int idx = 0; idx < utility.getNumberOfEvents(); idx++) {
-				if (idx < event)
-					sum[idx] = weight[idx];
-				else if (idx > event)
-					sum[idx - 1] = weight[idx];
-			}
-			result[event] = script.term("=", zero, collectTerms("+", sum, zero));
-		}
-
-		return Collections.singletonList(collectTerms("or", result, script.term("true")));
-	}
-
-	/**
-	 * Add the needed inequalities to guarantee that a distributable Petri Net region is calculated.
-	 * @param backwardWeight Terms representing the backward weights of transitions.
-	 * @return The needed terms.
-	 */
-	private List<Term> requireDistributableNet(Term[] backwardWeight) {
-		List<Term> result = new ArrayList<>();
-		Set<String> locations = new HashSet<>(Arrays.asList(locationMap));
-		locations.remove(null);
-		if (locations.isEmpty())
-			// No locations specified
-			return result;
-
-		Term zero = script.numeral(BigInteger.ZERO);
-		for (String location : locations) {
-			Term term = zero;
-
-			// Only events having location "location" may consume token.
-			for (int eventIndex = 0; eventIndex < utility.getNumberOfEvents(); eventIndex++) {
-				if (locationMap[eventIndex] != null && !locationMap[eventIndex].equals(location))
-					term = script.term("+", backwardWeight[eventIndex], term);
-			}
-
-			result.add(script.term("=", zero, term));
-		}
-
-		return result;
-	}
-
-	/**
-	 * Generate the necessary inequalities for a conflict free solution.
-	 * @param weight Terms representing the effective weights of transitions.
-	 * @param backwardWeight Terms representing the backward weights of transitions.
-	 * @return The needed terms.
-	 */
-	private List<Term> requireConflictFree(Term[] weight, Term[] backwardWeight) {
-		Term[] result = new Term[utility.getNumberOfEvents() + 1];
-		Term zero = script.numeral(BigInteger.ZERO);
-		Term sumOfAllBackwards = collectTerms("+", backwardWeight, zero);
-
-		// Conflict free: Either there is just a single transition consuming token...
-		// (And thus this automatically satisfies any distribution)
-		for (int event = 0; event < utility.getNumberOfEvents(); event++) {
-			Term[] sum = new Term[utility.getNumberOfEvents() - 1];
-			for (int idx = 0; idx < utility.getNumberOfEvents(); idx++) {
-				if (idx < event)
-					sum[idx] = backwardWeight[idx];
-				else if (idx > event)
-					sum[idx - 1] = backwardWeight[idx];
-			}
-			result[event] = script.term("=", zero, collectTerms("+", sum, zero));
-		}
-
-		// ...or the preset is contained in the postset
-		// (Note that this only works because we require plainness)
-		Term[] presetPostset = new Term[utility.getNumberOfEvents()];
-		for (int event = 0; event < utility.getNumberOfEvents(); event++) {
-			presetPostset[event] = script.term("<=", zero, weight[event]);
-		}
-		result[utility.getNumberOfEvents()] = collectTerms("and", presetPostset, script.term("false"));
-
-		return Collections.singletonList(collectTerms("or", result, script.term("true")));
 	}
 
 	/**
@@ -469,8 +162,8 @@ class InequalitySystemSeparation implements Separation {
 		try {
 			// We want r_S(s) != r_S(s'). Since for each region there exists a complementary region (we are
 			// only looking at the bounded case!), we can require r_S(s) < r_S(s')
-			Term term1 = evaluateReachingParikhVector(regionInitialMarking, regionWeights, state);
-			Term term2 = evaluateReachingParikhVector(regionInitialMarking, regionWeights, state);
+			Term term1 = helper.evaluateReachingParikhVector(regionInitialMarking, regionWeights, state);
+			Term term2 = helper.evaluateReachingParikhVector(regionInitialMarking, regionWeights, state);
 			script.assertTerm(script.term("<", term1, term2));
 
 			return regionFromSolution();
@@ -499,7 +192,7 @@ class InequalitySystemSeparation implements Separation {
 
 			// Each state must be reachable in the resulting region, but event 'event' should be disabled
 			// in state. We want -1 >= r_S(s) - r_B(event)
-			Term marking = evaluateReachingParikhVector(regionInitialMarking, regionWeights, state);
+			Term marking = helper.evaluateReachingParikhVector(regionInitialMarking, regionWeights, state);
 
 			Term term;
 			if (properties.isPure()) {
@@ -530,17 +223,6 @@ class InequalitySystemSeparation implements Separation {
 		Rational rat = (Rational) value;
 		assert rat.denominator().equals(BigInteger.ONE) : value;
 		return rat.numerator().intValue();
-	}
-
-	private Term collectTerms(String operation, Term[] terms, Term def) {
-		switch (terms.length) {
-			case 0:
-				return def;
-			case 1:
-				return terms[0];
-			default:
-				return script.term(operation, terms);
-		}
 	}
 }
 
