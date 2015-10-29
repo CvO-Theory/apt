5436e6e91f0e10e07c9c6a9a128784cb73abe096
vsp <vsp@chuck.Informatik.Uni-Oldenburg.DE>
2015-10-29 08:58:06 +0100

    parser: new antlr4 based synet parser
    Signed-off-by: vsp <vsp@chuck.Informatik.Uni-Oldenburg.DE>

--- src/uniol/apt/io/parser/impl/SynetPNParser.java
+++ src/uniol/apt/io/parser/impl/SynetPNParser.java
@@ -1,6 +1,6 @@
 /*-
  * APT - Analysis of Petri Nets and labeled Transition systems
- * Copyright (C) 2014  vsp
+ * Copyright (C) 2015  vsp
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,17 +19,27 @@
 
 package uniol.apt.io.parser.impl;
 
-import java.io.IOException;
-
 import java.io.InputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import uniol.apt.adt.exception.DatastructureException;
+import uniol.apt.adt.extension.Extensible;
 import uniol.apt.adt.pn.PetriNet;
-import uniol.apt.io.parser.ParseException;
+import uniol.apt.adt.pn.Place;
+import uniol.apt.adt.pn.Transition;
 import uniol.apt.io.parser.PNParser;
-import uniol.apt.io.parser.impl.synet.SynetPNFormatLexer;
-import uniol.apt.io.parser.impl.synet.SynetPNFormatParser;
-import uniol.apt.io.parser.impl.apt.APTParserContext;
-import uniol.apt.io.parser.impl.apt.APTPNParserOutput;
-import uniol.apt.io.parser.impl.exception.FormatException;
+import uniol.apt.io.parser.ParseException;
 
 /**
  * Petri net parser for the synet Petri Net format
@@ -37,16 +47,132 @@ import uniol.apt.io.parser.impl.exception.FormatException;
  * @author vsp
  */
 public class SynetPNParser extends AbstractPNParser implements PNParser {
-	@Override
-	public PetriNet parsePN(InputStream input) throws ParseException {
-		ParserContext<PetriNet> ctx = new APTParserContext<>(
-				SynetPNFormatLexer.class, SynetPNFormatParser.class,
-				APTPNParserOutput.class, "start");
-		try {
-			return new ANTLRParser().parse(input, ctx);
-		} catch (FormatException | IOException ex) {
-			throw new ParseException(ex);
+	private static class LocationListener extends SynetPNFormatBaseListener implements SynetPNFormatListener {
+		private final Set<String> locations;
+
+		private LocationListener(Set<String> locations) {
+			this.locations = locations;
 		}
+
+		@Override
+		public void exitLocation(SynetPNFormatParser.LocationContext ctx) {
+			String loc = ctx.loc.getText();
+
+			if (!this.locations.add(loc))
+				throw new ParseCancellationException(new ParseException("Location already exists."));
+		}
+	}
+
+	private static class PlaceTransitionListener extends SynetPNFormatBaseListener
+			implements SynetPNFormatListener {
+		private final PetriNet pn;
+		private final Set<String> locations;
+
+		private PlaceTransitionListener(PetriNet pn, Set<String> locations) {
+			this.pn        = pn;
+			this.locations = locations;
+		}
+
+		private static String extractPosition(ParserRuleContext ctx) {
+			Token token = ctx.getStart();
+			return "line " + token.getLine() + " pos " + token.getCharPositionInLine();
+		}
+
+		private void handleLocation(Extensible ext, SynetPNFormatParser.IdContext locCtx,
+				ParserRuleContext parentCtx) {
+			if (locCtx == null) {
+				if (!this.locations.isEmpty()) {
+					throw new ParseCancellationException(extractPosition(parentCtx)
+							+ " Missing Location");
+				} else {
+					return;
+				}
+			}
+
+			String locText = locCtx.getText();
+			if (!this.locations.contains(locText))
+				throw new ParseCancellationException(new ParseException(extractPosition(locCtx)
+							+ " Unknown Location"));
+			ext.putExtension("location", locText);
+		}
+
+		@Override
+		public void exitPlace(SynetPNFormatParser.PlaceContext ctx) {
+			Place p = this.pn.createPlace(ctx.p.getText());
+			handleLocation(p, ctx.loc, ctx);
+
+			if (ctx.init != null) {
+				long init = Integer.parseInt(ctx.init.getText());
+				p.setInitialToken(init);
+			}
+		}
+
+		@Override
+		public void exitTransition(SynetPNFormatParser.TransitionContext ctx) {
+			Transition t = this.pn.createTransition(ctx.t.getText());
+			handleLocation(t, ctx.loc, ctx);
+		}
+	}
+
+	private static class FlowListener extends SynetPNFormatBaseListener implements SynetPNFormatListener {
+		private final PetriNet pn;
+
+		private FlowListener(PetriNet pn) {
+			this.pn = pn;
+		}
+
+		@Override
+		public void exitFlowPreset(SynetPNFormatParser.FlowPresetContext ctx) {
+			int weight = 1;
+			if (ctx.w != null)
+				weight = Integer.parseInt(ctx.w.getText());
+
+			this.pn.createFlow(ctx.t.getText(), ctx.p.getText(), weight);
+		}
+
+		@Override
+		public void exitFlowPostset(SynetPNFormatParser.FlowPostsetContext ctx) {
+			int weight = 1;
+			if (ctx.w != null)
+				weight = Integer.parseInt(ctx.w.getText());
+
+			this.pn.createFlow(ctx.p.getText(), ctx.t.getText(), weight);
+		}
+	}
+
+	@Override
+	public PetriNet parsePN(InputStream is) throws ParseException {
+		CharStream input;
+		try {
+			input              = new ANTLRInputStream(is);
+		} catch (IOException ex) {
+			throw new ParseException(ex.getMessage(), ex);
+		}
+		SynetPNFormatLexer lexer   = new SynetPNFormatLexer(input);
+		lexer.removeErrorListeners(); // don't spam on stderr
+		lexer.addErrorListener(new ThrowingErrorListener());
+		CommonTokenStream tokens   = new CommonTokenStream(lexer);
+		SynetPNFormatParser parser = new SynetPNFormatParser(tokens);
+		parser.removeErrorListeners(); // don't spam on stderr
+		parser.addErrorListener(new ThrowingErrorListener());
+		parser.setBuildParseTree(true);
+		ParseTree tree;
+		try {
+			tree               = parser.pn();
+		} catch (ParseCancellationException ex) {
+			throw new ParseException(ex.getMessage(), ex);
+		}
+		PetriNet pn                = new PetriNet();
+		Set<String> locations      = new HashSet<>();
+		try {
+			ParseTreeWalker.DEFAULT.walk(new LocationListener(locations), tree);
+			ParseTreeWalker.DEFAULT.walk(new PlaceTransitionListener(pn, locations), tree);
+			ParseTreeWalker.DEFAULT.walk(new FlowListener(pn), tree);
+		} catch (DatastructureException | ParseCancellationException ex) {
+			throw new ParseException(ex.getMessage(), ex);
+		}
+
+		return pn;
 	}
 }
 
