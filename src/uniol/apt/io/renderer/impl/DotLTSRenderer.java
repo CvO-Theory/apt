2c69acda5561ce10b47d8d6422701439663789a2
vsp <vsp@chuck.Informatik.Uni-Oldenburg.DE>
2015-11-26 11:59:56 +0100

    DotLTSRenderer: new implementation which uses stringtemplate
    Signed-off-by: vsp <vsp@chuck.Informatik.Uni-Oldenburg.DE>

--- src/uniol/apt/io/renderer/impl/DotLTSRenderer.java
+++ src/uniol/apt/io/renderer/impl/DotLTSRenderer.java
@@ -1,6 +1,6 @@
 /*-
  * APT - Analysis of Petri Nets and labeled Transition systems
- * Copyright (C) 2012-2013  Members of the project group APT
+ * Copyright (C) 2015       vsp
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,62 +19,17 @@
 
 package uniol.apt.io.renderer.impl;
 
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Formatter;
-
-import uniol.apt.adt.ts.Arc;
-import uniol.apt.adt.ts.State;
 import uniol.apt.adt.ts.TransitionSystem;
 import uniol.apt.io.renderer.Renderer;
-import uniol.apt.io.renderer.RenderException;
 
 /**
  * This class converts Petri nets and transition systems to dot file strings.
  *
- *
- * @author Renke Grunwald
- *
+ * @author vsp
  */
-public class DotLTSRenderer extends AbstractRenderer<TransitionSystem> implements Renderer<TransitionSystem> {
-	private static final String TS_NODE_TEMPLATE =
-		"%1$s[label=\"%2$s\"];\n";
-	private static final String TS_INITIAL_NODE_TEMPLATE =
-		"%1$s[label=\"%2$s\", shape=circle];\n";
-	private static final String TS_EDGE_TEMPLATE =
-		"%1$s -> %2$s[label=\"%3$s\"];\n";
-
-	@Override
-	public void render(TransitionSystem ts, Writer writer) throws RenderException, IOException {
-		writer.append("digraph G {\n");
-		writer.append("node [shape = point, color=white, fontcolor=white]; start;");
-		writer.append("edge [fontsize=20]\n");
-		writer.append("node [fontsize=20,shape=circle,color=black, fontcolor=black, "
-				+ "height=0.5,width=0.5,fixedsize=true];\n");
-
-		Formatter nodeFormat = new Formatter(writer);
-
-		for (State node : ts.getNodes()) {
-			if (ts.getInitialState().equals(node)) {
-				nodeFormat.format(TS_INITIAL_NODE_TEMPLATE, node.getId(), node.getId());
-			} else {
-				nodeFormat.format(TS_NODE_TEMPLATE, node.getId(), node.getId());
-			}
-		}
-
-		nodeFormat.close();
-
-		Formatter edgeFormat = new Formatter(writer);
-
-		edgeFormat.format(TS_EDGE_TEMPLATE, "start", ts.getInitialState().getId(), "");
-
-		for (Arc edge : ts.getEdges()) {
-			edgeFormat.format(TS_EDGE_TEMPLATE, edge.getSource().getId(), edge.getTarget().getId(),
-					edge.getLabel());
-		}
-
-		edgeFormat.close();
-		writer.append("}\n");
+public class DotLTSRenderer extends AbstractSTRenderer<TransitionSystem> implements Renderer<TransitionSystem> {
+	public DotLTSRenderer() {
+		super("uniol/apt/io/renderer/impl/DotLTS.stg", "ts");
 	}
 }
 
