eaa73eeeaeae0fd07f48818004b3fee23d5ebea3
Jonas Prellberg <jonas.prellberg@uni-oldenburg.de>
2016-09-19 12:33:01 +0200

    Make ReversibleModule interruptible.
    The module is already interruptible without further changes to the
    implementing classes because the bulk of the time is spent constructing
    two SpanningTrees and the SpanningTree creation is already
    interruptible.

--- src/module/uniol/apt/analysis/reversible/ReversibleModule.java
+++ src/module/uniol/apt/analysis/reversible/ReversibleModule.java
@@ -24,10 +24,10 @@ import uniol.apt.adt.pn.Marking;
 import uniol.apt.adt.pn.PetriNet;
 import uniol.apt.adt.ts.State;
 import uniol.apt.adt.ts.TransitionSystem;
-import uniol.apt.module.AbstractModule;
+import uniol.apt.module.AbstractInterruptibleModule;
 import uniol.apt.module.AptModule;
 import uniol.apt.module.Category;
-import uniol.apt.module.Module;
+import uniol.apt.module.InterruptibleModule;
 import uniol.apt.module.ModuleInput;
 import uniol.apt.module.ModuleInputSpec;
 import uniol.apt.module.ModuleOutput;
@@ -39,7 +39,7 @@ import uniol.apt.module.exception.ModuleException;
  *
  */
 @AptModule
-public class ReversibleModule extends AbstractModule implements Module {
+public class ReversibleModule extends AbstractInterruptibleModule implements InterruptibleModule {
 
 	@Override
 	public String getName() {
