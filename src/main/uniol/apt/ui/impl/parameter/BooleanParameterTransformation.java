234376b4c705943c8472184ef2dcd438a3487d92
Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>
2017-02-15 12:37:47 +0100

    Add a BooleanParameterTransformation
    Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

---
+++ src/main/uniol/apt/ui/impl/parameter/BooleanParameterTransformation.java
@@ -0,0 +1,43 @@
+/*-
+ * APT - Analysis of Petri Nets and labeled Transition systems
+ * Copyright (C) 2017  Uli Schlachter
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package uniol.apt.ui.impl.parameter;
+
+import uniol.apt.ui.AptParameterTransformation;
+import uniol.apt.ui.ParameterTransformation;
+
+import uniol.apt.module.exception.ModuleException;
+
+/**
+ * Transform a string into a boolean value.
+ * @author Uli Schlachter
+ */
+@AptParameterTransformation(Boolean.class)
+public class BooleanParameterTransformation implements ParameterTransformation<Boolean> {
+	@Override
+	public Boolean transform(String arg) throws ModuleException {
+		if (arg.equalsIgnoreCase("true"))
+			return true;
+		if (arg.equalsIgnoreCase("false"))
+			return true;
+		throw new ModuleException("Not a valid boolean: " + arg);
+	}
+}
+
+// vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120
