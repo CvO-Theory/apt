CvO-Theory/apt

commit 2b05f6f9574f2063a6f9ecb728dc80416b0c46ce

Marking: optimize the copy-constructor
The function Marking#setMarking(Marking) is only called from the
copy-constructor. So this function does "the real work" of copying a marking.

Before this commit, it would ensure that the other marking is consistent, add
the entry of that other marking to this marking, and then make this marking
consistent. This latest step ensured that token for places which were since
removed are no longer saved.

However, this approach required iterating through all places of the Petri nets
in ensureConsistency() and checking if everything really was consistent.

This commit changes the setMarking() to clear its own map (all its entries will
be deleted or overwriten anyway) and then to copy the map from the other
marking. Afterwards, the place revision of the other marking is just copied to
this marking. No consistency is ensured.

With this patch it is possible that an inconsistent marking is created. However,
for this to happen, the input marking needed to be inconsistent as well. If
later on one of these marking is used, consistency will be ensured then. So this
case is handled just as well as before.

However, if the input marking was already consistent, then the whole logic of
ensureConsistency() will be skipped, because we now have a correct place
revision in the copy. This means that later on, when the marking is actually
used, ensureConsistency() will not have to do anything, too.

For the coverability module, this makes a big difference. Previously, lots of
copies of markings were created and all of these copies went through
ensureConsistency() during the copy-constructor. Now the initial Marking of the
Petri net will be made consistent once while the input PN is parsed and
afterwards ensureConsistency() will actually do anything anymore.

Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

src/uniol/apt/adt/pn/Marking.java
@@ -66,8 +66,7 @@ public class Marking {
 		this.net = m.net;
 		this.setMarking(m);
 	}
-        
-        
+
 	/**
 	 * Constructor.
 	 * <p/>
@@ -124,11 +123,11 @@ public class Marking {
 	 * @param m The marking that serves as template for copying the marking.
 	 */
 	private void setMarking(Marking m) {
-		m.ensureConsistency();
+		this.map.clear();
 		for (Map.Entry<String, Token> entry : m.map.entrySet()) {
 			this.map.put(entry.getKey(), new Token(entry.getValue()));
 		}
-		ensureConsistency();
+		this.rev = m.rev;
 	}
 
 	/**

commit 6d0491b6750e715477551d00027d7bcead376205

PetriNet: Test copy constuctor after a place was removed
This adds a new test to PetriNetTest which creates a place, gives that
place an initial token count, removes the place and finally tries to
copy the Petri net.

Currently, this will throw an extension since the constructor
Marking(PetriNet, Marking) will try to find the copy of the already
removed place and end up with an exception.

This is broken since commit 2b05f6f9574f2 which removed a call to
ensureConsistency() at the beginning of setMarking() (which later in
commit ccc71fd08560ea9 was inlined into this constructor).

Signed-off-by: Uli Schlachter <uli.schlachter@informatik.uni-oldenburg.de>

src/test/uniol/apt/adt/pn/PetriNetTest.java
@@ -291,6 +291,19 @@ public class PetriNetTest {
 		pn.createTransitions("t0", "t1");
 		pn.createFlow("t0", "t1");
 	}
+
+	@Test
+	public void testCopyAfterDelete() {
+		PetriNet pn = new PetriNet();
+		pn.createPlace("p").setInitialToken(42);
+		pn.removePlace("p");
+
+		// At this point, the initial marking of pn still contains "p". The next call to ensureConsistency()
+		// would clean that up, but let's instead copy the Petri net, which threw an exception at some point.
+
+		PetriNet copy = new PetriNet(pn);
+		assertTrue(copy.getNodes().isEmpty());
+	}
 }
 
 // vim: ft=java:noet:sw=8:sts=8:ts=8:tw=120